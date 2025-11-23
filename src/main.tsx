4b1a2236802d84e8c2b52bf9719a44b9f5b7dd24
malllayyyy <malayrc276@gmail.com>
2025-11-23 05:50:42 +0530

    chore: update source files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 )
